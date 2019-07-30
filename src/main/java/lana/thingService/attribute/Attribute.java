a99989e8a4d213c43ffe3955c53c6d37afb42c3f
lana <sitdownrightnow2552@gmail.com>
2019-07-30 20:10:46 +0700

    use lombok

--- src/main/java/lana/thingService/attribute/Attribute.java
+++ src/main/java/lana/thingService/attribute/Attribute.java
@@ -2,11 +2,17 @@ package lana.thingService.attribute;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lana.thingService.thing.Thing;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "attribute")
 @JsonIgnoreProperties(value = {"things"})
 public class Attribute {
@@ -20,41 +26,4 @@ public class Attribute {
 
     @OneToMany(mappedBy = "attribute")
     private List<Thing> things;
-
-    public Attribute() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-
-    public List<Thing> getThings() {
-        return things;
-    }
-
-
-    public void setThings(List<Thing> things) {
-        this.things = things;
-    }
 }
