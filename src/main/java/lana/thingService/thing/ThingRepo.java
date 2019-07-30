812d2a8453efa9638e3428ffa61825f87e4ecba6
lana <sitdownrightnow2552@gmail.com>
2019-07-30 20:04:51 +0700

    add fin by attribute

--- src/main/java/lana/thingService/thing/ThingRepo.java
+++ src/main/java/lana/thingService/thing/ThingRepo.java
@@ -1,9 +1,10 @@
 package lana.thingService.thing;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
 
 public interface ThingRepo extends PagingAndSortingRepository<Thing, Integer> {
-    List<Thing> findAllByAttribute_Id(int id);
+    Page<Thing> findAllByAttribute_Id(Integer id, Pageable pageable);
 }
