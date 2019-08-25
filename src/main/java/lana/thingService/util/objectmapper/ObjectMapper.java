d649b06d20231f64eb083618e8c7e014f33b53fd
sitdownrightnow2552@gmail.com <toilatoan283>
2019-08-25 17:58:08 +0700

    delete old files

--- src/main/java/lana/thingService/util/objectmapper/ObjectMapper.java
+++
@@ -1,7 +0,0 @@
-package lana.thingService.util.objectmapper;
-
-public interface ObjectMapper {
-    <T> T map(Object source, Class<T> destinationType);
-
-    void map(Object source, Object destination);
-}
